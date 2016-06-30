8ba91bf459e41a544f9eb0bed58c313032acf145
zonda600 <zonda600@gmail.com>
2016-06-30 13:28:55 -0500

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,12 @@
+#Read All Data
 SourceFile <- "household_power_consumption.txt"
 AllData <- read.table(SourceFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+#Subset the data for relevant months
 SubData <- AllData[AllData$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#str(SubData)
+#Create histogram from subet of data
 globalActivePower <- as.numeric(SubData$Global_active_power)
-png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
